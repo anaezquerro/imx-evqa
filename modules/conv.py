516abcadd809285300f307cd8bb42a3b54a9bc1a
anaezquerro <anaezquerro0701@gmail.com>
2024-03-08 22:07:30 +0100

    final submission

--- modules/conv.py
+++ modules/conv.py
@@ -4,19 +4,6 @@ import torch.nn.functional as F
 from modules.ffn import FFN 
 
 
-class ConvBlock(nn.Module):
-    def __init__(self, in_channels: int, out_channels: int):
-        super().__init__()
-        self.conv = nn.Sequential(
-            nn.Conv2d(in_channels, out_channels, kernel_size=3),
-            nn.BatchNorm2d(out_channels),
-            nn.LeakyReLU(inplace=True),
-            nn.MaxPool2d(2)
-        )
-    
-    def forward(self, imgs: torch.Tensor) -> torch.Tensor:
-        return self.conv(imgs)
-    
 
 class ConditionedConvBlock(nn.Module):
     BATCH_SIZE = 50
@@ -41,32 +28,6 @@ class ConditionedConvBlock(nn.Module):
         return feats
 
 
-# class ConditionedConvBlock(nn.Module):
-    
-#     def __init__(self, in_channels: int, out_channels: int, kernel_size: int, word_embed_size: int):
-#         super().__init__()
-#         self.conv = nn.Sequential(
-#             nn.Conv2d(in_channels, out_channels, kernel_size),
-#             nn.BatchNorm2d(out_channels),
-#             nn.LeakyReLU(inplace=True),
-#             nn.MaxPool2d(2)
-#         )
-#         self.ffn = None
-#         self.word_embed_size = word_embed_size
-
-    
-#     def forward(self, imgs: torch.Tensor, words: torch.Tensor) -> torch.Tensor:
-#         sizes = imgs.shape[-2:]
-#         if self.ffn is None:
-#             self.ffn = FFN(sizes[0]*sizes[1]+self.word_embed_size, sizes[0]*sizes[1])
-#         self.ffn = self.ffn.to(imgs.device)
-             
-#         # join imgs and words informations 
-#         condition = torch.concat([imgs.flatten(-2, -1), words.unsqueeze(1).repeat(1, imgs.shape[1], 1)], -1)
-#         feats = self.ffn(condition).view(imgs.shape[0], -1, sizes[0], sizes[1])
-#         feats = self.conv(feats)
-#         return feats
-
 class TransposedConvBlock(nn.Module):
     """Upscaling then double conv"""
 
@@ -80,96 +41,6 @@ class TransposedConvBlock(nn.Module):
     
     def forward(self, imgs: torch.Tensor) -> torch.Tensor:
         return self.up(imgs)
-
-class DoubleConv(nn.Module):
-    """(convolution => [BN] => ReLU) * 2"""
-
-    def __init__(self, in_channels, out_channels, mid_channels=None):
-        super().__init__()
-        if not mid_channels:
-            mid_channels = out_channels
-        self.double_conv = nn.Sequential(
-            nn.Conv2d(in_channels, mid_channels, kernel_size=3, padding=1, bias=False),
-            nn.BatchNorm2d(mid_channels),
-            nn.LeakyReLU(inplace=True),
-            nn.Conv2d(mid_channels, out_channels, kernel_size=3, padding=1, bias=False),
-            nn.BatchNorm2d(out_channels),
-            nn.LeakyReLU(inplace=True)
-        )
-
-    def forward(self, x):
-        return self.double_conv(x)
-
-
-class ConditionedDown(nn.Module):
-    BATCH_SIZE = 20
-    def __init__(self, in_channels, out_channels, word_embed_size: int):
-        super().__init__()
-        self.maxpool_conv = nn.Sequential(
-            nn.MaxPool2d(2),
-            DoubleConv(in_channels, out_channels)
-        )
-        self.attn = nn.MultiheadAttention(out_channels, num_heads=1, batch_first=True, kdim=word_embed_size, vdim=word_embed_size)
-
-    def forward(self, imgs: torch.Tensor, words: torch.Tensor):
-        feats = self.maxpool_conv(imgs)
-        sizes = feats.shape[-2:]
-        feats = feats.flatten(-2, -1).permute(0, 2, 1)
-        feats = torch.concat([self.attn(feats[:, i:(i+self.BATCH_SIZE), :], words, words)[0] for i in range(0, feats.shape[1], self.BATCH_SIZE)], 1)
-        return feats.view(feats.shape[0], *sizes, feats.shape[-1]).permute(0, 3, 1, 2)
-    
-class Down(nn.Module):
-    
-    def __init__(self, in_channels, out_channels):
-        super().__init__()
-        self.maxpool_conv = nn.Sequential(
-            nn.MaxPool2d(2),
-            DoubleConv(in_channels, out_channels)
-        )
-
-    def forward(self, imgs: torch.Tensor):
-        feats = self.maxpool_conv(imgs)
-        return feats
-
-class Up(nn.Module):
-    """Upscaling then double conv"""
-
-    def __init__(self, in_channels, out_channels, bilinear=True):
-        super().__init__()
-
-        # if bilinear, use the normal convolutions to reduce the number of channels
-        if bilinear:
-            self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
-            self.conv = DoubleConv(in_channels, out_channels, in_channels // 2)
-        else:
-            self.up = nn.ConvTranspose2d(in_channels, in_channels // 2, kernel_size=2, stride=2)
-            self.conv = DoubleConv(in_channels, out_channels)
-
-    def forward(self, x1, x2):
-        x1 = self.up(x1)
-        # input is CHW
-        diffY = x2.size()[2] - x1.size()[2]
-        diffX = x2.size()[3] - x1.size()[3]
-
-        x1 = F.pad(x1, [diffX // 2, diffX - diffX // 2,
-                        diffY // 2, diffY - diffY // 2])
-        # if you have padding issues, see
-        # https://github.com/HaiyongJiang/U-Net-Pytorch-Unstructured-Buggy/commit/0e854509c2cea854e247a9c615f175f76fbb2e3a
-        # https://github.com/xiaopeng-liao/Pytorch-UNet/commit/8ebac70e633bac59fc22bb5195e513d5832fb3bd
-        x = torch.cat([x2, x1], dim=1)
-        return self.conv(x)
-
-
-class OutConv(nn.Module):
-    def __init__(self, in_channels, act: nn.Module = nn.Sigmoid()):
-        super(OutConv, self).__init__()
-        self.conv = nn.Conv2d(in_channels, 1, kernel_size=1)
-        self.act = act
-
-    def forward(self, x):
-        x = self.conv(x)
-        return normalize(x)
-    
 
 
 def normalize(x: torch.Tensor):
